97d51c5d367f485e14bc0ad173e0d6c932bc8a45
suumoo5 <laureano.cavallo@gmail.com>
2021-05-12 17:20:56 -0300

    Comento todos los errores que veo a simple vista. Van a salir mas cuando agregue un TipoMovimiento y haga lo que pueda polimorficamente

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -11,9 +11,10 @@ import java.util.List;
 
 public class Cuenta {
 
-  private double saldo = 0;
+  private double saldo = 0; //No inicializar variables, sino en constructores
   private List<Movimiento> movimientos = new ArrayList<>();
 
+  //Arreglar los Constructors. Unirlos
   public Cuenta() {
     saldo = 0;
   }
@@ -22,46 +23,44 @@ public class Cuenta {
     saldo = montoInicial;
   }
 
-  public void setMovimientos(List<Movimiento> movimientos) {
-    this.movimientos = movimientos;
-  }
-
   public void poner(double cuanto) {
     if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
 
-    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {
+    if (getMovimientos().stream().filter(movimiento -> movimiento.isDeposito()).count() >= 3) {//se puede aclarar usando Extraccion y Deposito. Podria hacer esa consulta el Movimiento o en algun TipoMovimiento?
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
 
-    new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);
+    new Movimiento(LocalDate.now(), cuanto, true).agregateA(this); // ??
   }
 
   public void sacar(double cuanto) {
     if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
-    if (getSaldo() - cuanto < 0) {
+    if (getSaldo() - cuanto < 0) {  //Podria ser una funcion, para mas claridad.
       throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
     }
+
+    //aca abajo hay algo sumamente raro
     double montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
     double limite = 1000 - montoExtraidoHoy;
     if (cuanto > limite) {
       throw new MaximoExtraccionDiarioException("No puede extraer mas de $ " + 1000
           + " diarios, límite: " + limite);
     }
-    new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);
+    new Movimiento(LocalDate.now(), cuanto, false).agregateA(this); // ??
   }
 
-  public void agregarMovimiento(LocalDate fecha, double cuanto, boolean esDeposito) {
+  public void agregarMovimiento(LocalDate fecha, double cuanto, boolean esDeposito) { //Pasarle el movimiento ya armado, y no sus variables
     Movimiento movimiento = new Movimiento(fecha, cuanto, esDeposito);
     movimientos.add(movimiento);
   }
 
   public double getMontoExtraidoA(LocalDate fecha) {
     return getMovimientos().stream()
-        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
+        .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha)) //se puede aclarar usando Extraccion y Deposito. Podria hacer esa consulta el Movimiento o en algun TipoMovimiento?
         .mapToDouble(Movimiento::getMonto)
         .sum();
   }
@@ -69,11 +68,10 @@ public class Cuenta {
   public List<Movimiento> getMovimientos() {
     return movimientos;
   }
-
+  public void setMovimientos(List<Movimiento> movimientos) {this.movimientos = movimientos;} //No se que tan util es este setter
   public double getSaldo() {
     return saldo;
   }
-
   public void setSaldo(double saldo) {
     this.saldo = saldo;
   }
--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -2,12 +2,13 @@ package dds.monedero.model;
 
 import java.time.LocalDate;
 
+//Dividir a Movimiento en Extraccion y Deposito. Clase TipoMovimiento?
 public class Movimiento {
   private LocalDate fecha;
   //En ningún lenguaje de programación usen jamás doubles para modelar dinero en el mundo real
   //siempre usen numeros de precision arbitraria, como BigDecimal en Java y similares
   private double monto;
-  private boolean esDeposito;
+  private boolean esDeposito; //Esto vuela. Reemplazarlo por TipoMovimiento (clase? Interface?)
 
   public Movimiento(LocalDate fecha, double monto, boolean esDeposito) {
     this.fecha = fecha;
@@ -15,21 +16,9 @@ public class Movimiento {
     this.esDeposito = esDeposito;
   }
 
-  public double getMonto() {
-    return monto;
-  }
+  public boolean fueDepositado(LocalDate fecha) {return isDeposito() && esDeLaFecha(fecha);} //Haciendo Extraccion y Deposito habria que ver de nuevo esta funcion
 
-  public LocalDate getFecha() {
-    return fecha;
-  }
-
-  public boolean fueDepositado(LocalDate fecha) {
-    return isDeposito() && esDeLaFecha(fecha);
-  }
-
-  public boolean fueExtraido(LocalDate fecha) {
-    return isExtraccion() && esDeLaFecha(fecha);
-  }
+  public boolean fueExtraido(LocalDate fecha) {return isExtraccion() && esDeLaFecha(fecha);} //Haciendo Extraccion y Deposito habria que ver de nuevo esta funcion
 
   public boolean esDeLaFecha(LocalDate fecha) {
     return this.fecha.equals(fecha);
@@ -37,17 +26,19 @@ public class Movimiento {
 
   public boolean isDeposito() {
     return esDeposito;
-  }
+  } // Red flag --> vuela
 
   public boolean isExtraccion() {
     return !esDeposito;
-  }
+  } // Red flag --> vuela
 
+  //Agregar directamente el Movimiento
   public void agregateA(Cuenta cuenta) {
     cuenta.setSaldo(calcularValor(cuenta));
     cuenta.agregarMovimiento(fecha, monto, esDeposito);
   }
 
+  //Podria hacerse polimorifica si existe Extraccion y Deposito
   public double calcularValor(Cuenta cuenta) {
     if (esDeposito) {
       return cuenta.getSaldo() + getMonto();
@@ -55,4 +46,16 @@ public class Movimiento {
       return cuenta.getSaldo() - getMonto();
     }
   }
+
+  public double getMonto() {
+    return monto;
+  }
+  public LocalDate getFecha() {
+    return fecha;
+  }
 }
+
+/* Notas
+- Cuenta ya conoce a Movimiento, sin embargo, Movimiento pasa a Cuenta por paramentro en algunos metodos
+
+ */
